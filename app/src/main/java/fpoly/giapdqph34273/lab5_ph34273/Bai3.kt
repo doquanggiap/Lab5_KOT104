deac063db9b033d0fe46772ce87269bcdcf68095
giapdq04 <167546581+giapdq04@users.noreply.github.com>
2024-05-29 16:15:25 +0700

    cập nhật bài 3 🚀🚀🚀

--- app/src/main/java/fpoly/giapdqph34273/lab5_ph34273/Bai3.kt
+++ app/src/main/java/fpoly/giapdqph34273/lab5_ph34273/Bai3.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
@@ -28,6 +29,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -40,13 +43,16 @@ fun Bai3() {
 @Composable
 fun CategoryApp() {
     val categories = listOf(
-        "Fiction", "Mystery", "Science Fiction", "Fantasy", "Adventure", "Historical", "Horror",
-        "Romance"
+        "Fiction", "Mystery", "Science Fiction",
+        "Fantasy", "Adventure", "Historical",
+        "Horror", "Romance"
     )
+
     val suggestions = listOf(
         "Biography", "Cookbook", "Poetry",
         "Self-help", "Thriller"
     )
+
     var selectedCategories by remember {
         mutableStateOf(setOf<String>())
     }
@@ -60,8 +66,10 @@ fun CategoryApp() {
             )
     ) {
 
-        Text("Choose a category:", style =
-        MaterialTheme.typography.titleMedium)
+        Text(
+            "Choose a category:", style =
+            MaterialTheme.typography.titleMedium
+        )
         Spacer(modifier = Modifier.height(8.dp))
         AssistChip(
             onClick = { /* Do something */ },
@@ -92,14 +100,28 @@ fun CategoryApp() {
             }
         )
         Spacer(modifier = Modifier.height(16.dp))
-        SelectedCategoriesChips(
-            selectedCategories = selectedCategories,
-            onCategoryRemoved = { category ->
-                selectedCategories = selectedCategories.toMutableSet().apply {
-                    remove(category)
-                }
-            }
-        )
+        if (selectedCategories.isNotEmpty()) {
+            Spacer(modifier = Modifier.height(16.dp))
+            SelectedCategoriesChips(selectedCategories, onCategoryRemoved =
+            { category ->
+                selectedCategories = selectedCategories - category
+            })
+            Spacer(modifier = Modifier.height(4.dp))
+            AssistChip(
+                onClick = { selectedCategories = setOf() },
+                label = {
+                    Text(
+                        "Clear selections",
+                        style = TextStyle(color = Color.White, fontWeight =
+                        FontWeight.Bold)
+                    )
+                },
+                colors = AssistChipDefaults.assistChipColors(containerColor
+                = Color.DarkGray),
+                border = AssistChipDefaults.assistChipBorder(borderColor =
+                Color.Black)
+            )
+        }
     }
 }
 
@@ -171,9 +193,11 @@ fun SelectedCategoriesChips(
                     Icon(
                         imageVector = Icons.Filled.Close,
                         contentDescription = "Deselect",
-                        modifier = Modifier.clickable {
-                            onCategoryRemoved(selectedCategory)
-                        }.size(18.dp)
+                        modifier = Modifier
+                            .clickable {
+                                onCategoryRemoved(selectedCategory)
+                            }
+                            .size(18.dp)
                     )
                 },
                 modifier = Modifier.padding(end = 8.dp),
